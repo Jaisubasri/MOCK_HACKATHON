8d6dc911f4bc60565c5b96c6d576dc98362bdd81
Jaisubasri <jaisu17k@gmail.com>
2024-01-06 16:32:50 +0530

    Initial commit

--- level_1a.py
+++ level_1a.py
@@ -2,63 +2,67 @@ import json
 import itertools
 
 f = open('Input data/level1a.json')
-d = json.load(f)
-neighbour=d['neighbourhoods']
+data = json.load(f)
+neighbour=data['neighbourhoods']
 #let us define two lists to hold order quantity and distances
 ord_quantity=[]
-distance=[]
+dist=[]
 for i in neighbour.keys():
     temp=list(neighbour[i].values())
     ord_quantity.append(temp[0])
-    distance.append(temp[1])
+    dist.append(temp[1])
 
-res_dist=d['restaurants']['r0']['neighbourhood_distance']
-max_cap=d['vehicles']['v0']['capacity']
+res_dist=data['restaurants']['r0']['neighbourhood_distance']
+max_cap=data['vehicles']['v0']['capacity']
 
-def knapsack(distance, res_dist, ord_quantities, max_cap):
-    node_order = sorted(range(len(res_dist)), key=lambda x: res_dist[x])
-    slots = []
-    current_slot = []
-    current_capacity = 0
-    current_distance = 0
-
-    for node in node_order:
-        if current_capacity + ord_quantities[node] <= max_cap:
-            current_slot.append(node)
-            current_capacity += ord_quantities[node]
-
-            if len(current_slot) > 1:
-                current_distance += distance[current_slot[-2]][current_slot[-1]]
-        else:
-            slots.append((current_slot, current_distance))
-            current_slot = [node]
-            current_capacity = ord_quantities[node]
-            current_distance = 0
-
-    if current_slot:
-        slots.append((current_slot, current_distance))
-
+def optimized(slots,dist):
     optimized_slots = []
 
     for slot, _ in slots:
         possible_orders = itertools.permutations(slot)
-        min_distance = float('inf')
+        min_dist = float('inf')
         best_order = []
 
         for order in possible_orders:
-            dist = sum(distance[order[i]][order[i+1]] for i in range(len(order) - 1))
-            if dist < min_distance:
-                min_distance = dist
+            dists = sum(dist[order[i]][order[i+1]] for i in range(len(order) - 1))
+            if dists < min_dist:
+                min_distance = dists
                 best_order = order
 
-        optimized_slots.append((best_order, min_distance))
+        optimized_slots.append((best_order, min_dist))
 
     return optimized_slots
-os=knapsack(distance, res_dist, ord_quantity,max_cap)
+
+def knapsack_tps(dist, res_dist, ord_quantities, max_capacity):
+    node_order = sorted(range(len(res_dist)), key=lambda x: res_dist[x])
+    slots = []
+    current_slot = []
+    current_capacity = 0
+    current_dist = 0
+
+    for node in node_order:
+        if current_capacity + ord_quantities[node] <= max_capacity:
+            current_slot.append(node)
+            current_capacity += ord_quantities[node]
+
+            if len(current_slot) > 1:
+                current_dist += dist[current_slot[-2]][current_slot[-1]]
+        else:
+            slots.append((current_slot, current_dist))
+            current_slot = [node]
+            current_capacity = ord_quantities[node]
+            current_dist = 0
+
+    if current_slot:
+        slots.append((current_slot, current_dist))
+
+    return optimized(slots,dist)
+    
+val=knapsack_tps(dist, res_dist, ord_quantity,max_cap)
 
 #output file coding 
 final_lst=[]
-for i in os:
+for i in val:
     temp=[]
     temp.append('r0')
     for j in i[0]:
